6707e234af2c795d55d960862487bc68137f6a78
Martín Ponce <martinp88@gmail.com>
2024-06-26 01:14:01 -0300

    migrate to react app

---
+++ calendar-app/src/components/EventList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const EventList = ({ events, onDeleteEvent }) => {
+    return (
+        <ul id="events">
+            {events.map((event, index) => (
+                <li key={`${event.date} ${event.time} ${event.name}`}>
+                    {event.date} {event.time}: {event.name}
+                    <button onClick={() => onDeleteEvent(event.date, event.time, index)}>
+                        Eliminar
+                    </button>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+export default EventList;
