a3f3c04f0706d16b858c9293ac91794f3c7a48d0
Martín Ponce <martinp88@gmail.com>
2024-06-30 09:34:24 -0300

    Added a text for when there are no events

--- calendar-app/src/components/EventList.js
+++ calendar-app/src/components/EventList.js
@@ -3,17 +3,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const EventList = ({ events, onDeleteEvent }) => {
-   return (
-    <ul>
-      {events.map((event, index) => (
-        <li key={`${event.date}-${event.time}-${index}`}>
-          {event.date} {event.time}: {event.name}
-          <button onClick={() => onDeleteEvent(event.date, event.time, index)}>
-          <FontAwesomeIcon icon={faTrash} />
-          </button>
-        </li>
-      ))}
-    </ul>
+  return (
+    <div>
+      {events.length === 0 ? (
+        <p>No hay eventos</p>
+      ) : (
+        <ul>
+          {events.map((event, index) => (
+            <li key={`${event.date}-${event.time}-${index}`}>
+              {event.date} {event.time}: {event.name}
+              <button onClick={() => onDeleteEvent(event.date, event.time, index)}>
+                <FontAwesomeIcon icon={faTrash} />
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
